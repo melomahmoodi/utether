eb4949c1e9a0acd480aa79bb2c671e6cc7b6ccb0
melikamahmoudi077@gmail.com <161129410+melomahmoodi@users.noreply.github.com>
2024-08-12 11:52:30 +0330

    Update

--- components/Pages/index.tsx
+++ components/Pages/index.tsx
@@ -11,14 +11,69 @@ export default p => Component(p, Page);
 const Page: PageEl = (props, state, refresh, getProps) => {
 
   let styles = global.styles
-  let name = "خوش آمدید"
 
 
   return (
-    <div style={{ direction: "rtl", minHeight: "11vh", }}>
+    <div style={{ direction: "rtl", minHeight: "11vh", borderRadius : 0 }}>
       <br-x />
-      <Window title={name} style={{ minHeight: 200, margin: 10, width: "calc(100% - 20px)" }}>
-        <pre style={{ direction: "ltr" }}>{JSON.stringify(props, null, 2)}</pre>
+      <br-xx/>
+      <br-xx/>
+
+      <Window title={"قیمت لحظه ای تتر:"} style={{ minHeight: 200, margin: 10, width: "calc(100% - 10px)" }}>
+      <br-xx/>
+
+       <div style={{width:"100%" , height:50 , backgroundColor:"#FF54DA" , borderRadius:0 , 
+        textAlign:"center"
+       }}>
+        <br-x/>
+        <br-xx/>
+        <br-xx/>
+        
+        قیمت لحظه ای:{(props.p.price as number).toLocaleString("fa-IR")}
+       </div>
+
+       <br-xx/>
+       
+       
+
+       <div style={{width:"100%" , height:50 , backgroundColor:"#FF54DA" , borderRadius:0 , 
+        textAlign:"center"
+       }}>
+        <br-x/>
+        <br-xx/>
+        تغییرات در ۲۴ ساعت گذشته: ٪ {(Number(props.p.diff24d) as number).toLocaleString("fa-IR")} 
+       </div>
+
+              <br-xx/>
+       
+
+       <div style={{width:"100%" , height:50 , backgroundColor:"#FF54DA" , borderRadius:0 , 
+        textAlign:"center"
+       }}>
+        <br-x/>
+        <br-xx/>
+        تغییرات در ۷ روز گذشته: ٪ {(Number(props.p.diff7d) as number).toLocaleString("fa-IR")} 
+       </div>
+
+
+       <br-xx/>
+       
+
+       <div style={{width:"100%" , height:50 , backgroundColor:"#FF54DA" , borderRadius:0 , 
+        textAlign:"center"
+       }}>
+        <br-x/>
+        <br-xx/>
+       تغییرات در ۱ ماه گذشته: ٪ {(Number(props.p.diff30d) as number).toLocaleString("fa-IR")} 
+       </div>
+       <br-xx/>
+        
+        <center style={{fontSize:11}}>
+        تهیه شده توسط گروه kepler
+        </center>
+       <br-xx/>
+         
+
       </Window>
     </div>
   )
@@ -27,17 +82,21 @@ const Page: PageEl = (props, state, refresh, getProps) => {
 
 export async function getServerSideProps(context) {
 
-
   var session = await global.SSRVerify(context)
   var { uid, name, image, imageprop, lang, cchar,
     unit, workspace, servid, servsecret,
     usedquota, quota, quotaunit, status, regdate, expid,
     role, path, devmod, userip, } = session;
 
+    let res = (await fetch("https://api.tetherland.com/currencies"))
+    let data = await res.json()
+    let p = data.data.currencies.USDT
+    console.log("PRICEEEE:", p)
 
   return {
     props: {
       data: global.QSON.stringify({
+        p,
         session,
         // nlangs,
       })
